b08cf8c9c9f23d880aa0da01844b8f730967468b
ghigbie <gehigbie@gmail.com>
2019-03-12 20:34:42 -0600

    Add form input features

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import (StringField, BooleanField, DateTimeField, 
+from wtforms import (StringField, BooleanField, DateTimeField, SelectField
                      RadioField, TextField, TextAreaField, SubmitField)
 from wtforms.validators import DataRequired
 
@@ -8,4 +8,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 title = 'Flask WTForms - More Practice'
 
-
+class InfoForm(FlaskForm):
+    breed = StringField('What breed our you?', validators=[DataRequired()])
+    neutered = BooleanField("Have you been neutered?")
+    mood = RadioField("Please choose your mood:",
+                      choices=[('mood_one', 'Happy'), ('mood_two', 'Excited'), ('mood_three', 'Sleepy')])
+    food_choice = SelectField(u"Pick your favorite food: ",
+                              choices=[('chi', 'chicken'), ('bf', 'beef'), ('pk', 'pork')])
