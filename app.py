e89b256fe418089a85e19cbecd563d4443417b76
ghigbie <gehigbie@gmail.com>
2019-03-12 21:09:40 -0600

    Edit form layout

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 title = 'Flask WTForms - More Practice'
 
 class InfoForm(FlaskForm):
-    breed = StringField('What breed our you?', validators=[DataRequired()])
-    neutered = BooleanField("Have you been neutered?")
-    mood = RadioField("Please choose your mood:",
+    breed = StringField('What breed our you? ', validators=[DataRequired()])
+    neutered = BooleanField("Have you been neutered? ")
+    mood = RadioField("Please choose your mood: ",
                       choices=[('mood_one', 'Happy'), ('mood_two', 'Excited'), ('mood_three', 'Sleepy')])
     food_choice = SelectField(u"Pick your favorite food: ",
                               choices=[('chi', 'Chicken'), ('bf', 'Beef'), ('pk', 'Pork'), ('fish', 'Fish') ])
-    feedback = TextAreaField()
+    feedback = TextAreaField("Please provide any feedback: ")
     submit = SubmitField("Submit Info")
 
 @app.route('/', methods=['GET', 'POST'])
