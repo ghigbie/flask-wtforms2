b3b1a73d18bdc6d06ed700f6362ce7b709fe92f0
ghigbie <gehigbie@gmail.com>
2019-03-12 20:26:51 -0600

    Add more configuration

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, DateTimeField, RadioField, TextField, TextAreaField, SubmitField
+from wtforms import (StringField, BooleanField, DateTimeField, 
+                     RadioField, TextField, TextAreaField, SubmitField)
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+title = 'Flask WTForms - More Practice'
+
+
